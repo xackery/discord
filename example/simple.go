cb258efbef94a33ea1b773929e514f639467fe2a
Xackery Xtal <xackery@gmail.com>
2015-09-01 20:58:22 -0700

    Added comments to my simple example

--- example/simple.go
+++ example/simple.go
@@ -6,21 +6,28 @@ import (
 )
 
 func main() {
-	c, e := discord.Create("email", "password")
-	fmt.Println(c.Session.Id)
+	//Create a new httpClient
+	restClient, e := discord.Create("email", "password")
 	if e != nil {
 		panic(e.Error())
 	}
-	guilds, e := discord.ListGuilds(c)
-	/*if e != nil {
+
+	//Get list of guilds
+	guilds, e := discord.ListGuilds(restClient)
+	if e != nil {
 		panic(e.Error())
-	}*/
-	guildId := "0"
-	if len(guilds) == 1 {
-		guildId = guilds[0].Id
 	}
 
-	channels, e := discord.ListChannels(c, guildId)
+	guildId := "0"
+	if len(guilds) < 1 {
+		panic("No guilds returned..!")
+	}
+
+	//Just snag initial guild
+	guildId = guilds[0].Id
+
+	//Get list of channels
+	channels, e := discord.ListChannels(restClient, guildId)
 	channelId := "0"
 	for _, channel := range channels {
 		if channel.Name == "lineage2" {
@@ -28,12 +35,16 @@ func main() {
 			channelId = channel.Id
 		}
 	}
-	if channelId == "0" {
-		fmt.Println("Fail!")
-		return
-	}
-	message, e := discord.SendMessage(c, channelId, "Capturing the struct of message responses..")
-	fmt.Println(message)
 
+	//I'm looking for a specific channel, not found? error here.
+	if channelId == "0" {
+		panic("Fail!?")
+	}
+
+	//Send a message
+	message, e := discord.SendMessage(restClient, channelId, "Capturing the struct of message responses..")
+
+	//output example stuff.
+	fmt.Println(message)
 	fmt.Println("Done", guildId, channelId)
 }
