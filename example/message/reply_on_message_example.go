adeb312ed587f3fdbf5f1bef91ef436a7f7f350d
Xackery Xtal <xackery@gmail.com>
2015-11-07 02:30:32 -0800

    Made my examples more verbose with comments.

--- example/message/reply_on_message_example.go
+++ example/message/reply_on_message_example.go
@@ -7,6 +7,7 @@ import (
 
 var client discord.Client
 
+//Whenever a message creation event triggers, this function is notified
 func OnMessageCreateExample(event discord.Event, message discord.Message) {
 	if message.Author.ID == client.ID {
 		log.Println("Ignoring myself")
@@ -14,19 +15,26 @@ func OnMessageCreateExample(event discord.Event, message discord.Message) {
 	}
 	log.Println("Got message!", message.ChannelID, client.Token, message.Author.Username, message.Content)
 
+	//Reply to the event with a message
 	message, err := client.ChannelMessageSend(message.ChannelID, "Hi "+message.Author.Username+", I see you.")
 	if err != nil {
 		log.Println("Err message sending:", err.Error())
 	}
 }
 
+//This is the equivalent of main()
 func ReplyOnMessageExample(email string, password string) {
+	//Log in to discord
 	err := client.Login(email, password)
 	if err != nil {
 		log.Println("Error Logging in:", err.Error())
 		return
 	}
+	//Register an event handler
 	client.OnMessageCreate = OnMessageCreateExample
+
+	//Listen for events.
+	//This code is blocked and will never end until client.StopListen() is called.
 	err = client.Listen()
 	if err != nil {
 		log.Println("Error Listening:", err.Error())
--- example/message/send_message_example.go
+++ example/message/send_message_example.go
@@ -4,17 +4,21 @@ import (
 	"log"
 )
 
+//This is the equivalent of main()
 func SendMessageExample(email string, password string) {
+	//Log in to discord
 	err := client.Login(email, password)
 	if err != nil {
 		log.Println("Error Logging in:", err.Error())
 		return
 	}
 
+	//Get a list of guilds (servers)
 	guilds, err := client.UserGuilds()
 	if err != nil {
 		log.Println("Error getting guilds:", err.Error())
 	}
+
 	log.Println("Guild size:", len(guilds))
 	if len(guilds) < 1 {
 		log.Println("no guilds, exiting")
@@ -22,12 +26,16 @@ func SendMessageExample(email string, password string) {
 	}
 
 	guild := guilds[0]
+
+	//Get a list of channels (rooms)
 	channels, err := client.GuildChannels(guild.ID)
 	if err != nil {
 		log.Println("Error getting channels:", err.Error())
 		return
 	}
 	log.Println("Channel size:", len(channels))
+
+	//Find the channel "test"
 	for _, channel := range channels {
 		if channel.Name == "test" {
 			log.Println("Sending message to ", channel.ID)
