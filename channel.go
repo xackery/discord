7fdc822309fc6cddfea8928f2c786adc6310d47e
Xackery Xtal <xackery@gmail.com>
2015-11-07 01:38:35 -0800

    Added changes to channel, so more fields are detected properly

--- channel.go
+++ channel.go
@@ -12,14 +12,14 @@ import (
 
 //Channels are sections inside guilds that messages are grouped by
 type Channel struct {
-	GuildID              int `json:",string,omitempty"`
+	GuildID              int `json:"guild_id,string,omitempty"`
 	Name                 string
-	PermissionOverWrites []PermissionOverWrites `json:"permission_overwrites,omitempty"`
+	PermissionOverwrites []PermissionOverwrites `json:"permission_overwrites,omitempty"`
 	Topic                string
-	Position             int `json:",omitempty"`
-	LastMessageID        string
+	Position             int    `json:",omitempty"`
+	LastMessageID        string `json:"last_message_id"`
 	Type                 string
-	Id                   int `json:",string,omitempty"`
+	ID                   int `json:",string,omitempty"`
 	IsPrivate            bool
 }
 
@@ -31,46 +31,11 @@ type PrivateChannel struct {
 }
 
 //Channel setting regarding if a permission overwrites
-type PermissionOverWrites struct {
-}
-
-//Posted message inside a channel
-type Message struct {
-	Attachments     []Attachment
-	Tts             bool
-	Embeds          []Embed
-	Timestamp       string
-	MentionEveryone bool
-	ID              int `json:",string,omitempty"`
-	EditedTimestamp string
-	Author          *Author
-	Content         string
-	ChannelID       int `json:",string,omitempty"`
-	Mentions        []Mention
-}
-
-//Mentions are @user inside a message
-type Mention struct {
-}
-
-//Attachment array entry
-type Attachment struct {
-}
-
-//Embedded media array entry
-type Embed struct {
-}
-
-//An author of a message
-type Author struct {
-	Username      string
-	Discriminator int `json:",string"`
-	ID            int `json:",string"`
-	Avatar        string
+type PermissionOverwrites struct {
 }
 
 //Send a message to specified channel
-func (c Client) ChannelMessageSend(channelId int, messageText string) (responseMessage Message, err error) {
+func (c *Client) ChannelMessageSend(channelId int, messageText string) (responseMessage Message, err error) {
 	if !c.IsLoggedIn() {
 		err = errors.New("You must be logged in")
 		return
